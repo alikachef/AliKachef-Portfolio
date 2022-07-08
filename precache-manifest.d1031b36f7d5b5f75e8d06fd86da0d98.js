ae307ec2473cf76c8419ac104563e4b62edf562e
GesSpark <kachefali@gmail.com>
2022-07-08 14:54:00 -0400

    Updates

---
+++ precache-manifest.d1031b36f7d5b5f75e8d06fd86da0d98.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f633e531360d463ae7d57ea1c9881f",
+    "url": "/AliKachef-Portfolio/index.html"
+  },
+  {
+    "revision": "6e288622d388d1ea9d8f",
+    "url": "/AliKachef-Portfolio/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "963f289146e09a95d425",
+    "url": "/AliKachef-Portfolio/static/css/main.dcaf3b49.chunk.css"
+  },
+  {
+    "revision": "6e288622d388d1ea9d8f",
+    "url": "/AliKachef-Portfolio/static/js/2.4d7eef9b.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/AliKachef-Portfolio/static/js/2.4d7eef9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "963f289146e09a95d425",
+    "url": "/AliKachef-Portfolio/static/js/main.10b59a30.chunk.js"
+  },
+  {
+    "revision": "5546ad94780653a7e504",
+    "url": "/AliKachef-Portfolio/static/js/runtime-main.dcd7f930.js"
+  },
+  {
+    "revision": "21c4a03af9e898a962cd3f99befd6c0e",
+    "url": "/AliKachef-Portfolio/static/media/2.21c4a03a.png"
+  },
+  {
+    "revision": "5eef4e1c8eb8b1738955f0a1f58f9660",
+    "url": "/AliKachef-Portfolio/static/media/396534.5eef4e1c.jpg"
+  },
+  {
+    "revision": "cd5be99818418b53037148c4476d9da7",
+    "url": "/AliKachef-Portfolio/static/media/CRUDApp.cd5be998.png"
+  },
+  {
+    "revision": "cda4f71a132596e37407cc246d609bb5",
+    "url": "/AliKachef-Portfolio/static/media/MembershipM.cda4f71a.png"
+  },
+  {
+    "revision": "0976620c8ba7b4f9145827009767482e",
+    "url": "/AliKachef-Portfolio/static/media/chatRoom.0976620c.png"
+  },
+  {
+    "revision": "a55ae079dfcae783d05b7c82a292e5ee",
+    "url": "/AliKachef-Portfolio/static/media/eureka (1).a55ae079.png"
+  },
+  {
+    "revision": "cdf9cd8bde01ccf080d9f4dea125536b",
+    "url": "/AliKachef-Portfolio/static/media/form.cdf9cd8b.png"
+  },
+  {
+    "revision": "78173ab476ea3f6bd5c12b2a514e3596",
+    "url": "/AliKachef-Portfolio/static/media/hvg.78173ab4.png"
+  }
+]);
